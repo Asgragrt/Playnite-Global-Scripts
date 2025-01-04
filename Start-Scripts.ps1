97571885ca63da2efb4dfb11ebe7a745f201dd96
Asgragrt <131921758+Asgragrt@users.noreply.github.com>
2025-01-03 18:04:58 -0600

    🎨 Change call

--- Start-Scripts.ps1
+++ Start-Scripts.ps1
@@ -1,32 +1,33 @@
+param(
+    [Parameter(Mandatory)]
+    [string]$Directory
+)
+
 $ValidDirs = @("pre", "post", "exit")
 
-function Start-Scripts {
-    param([string]$Directory)
-
-    if ([string]::IsNullOrWhiteSpace($Directory)) {
-        throw "Start-Scripts: Please specify the value of '`$ScriptDir'!"
-    }
-
-    $ScriptDir = $Directory.toLower()
-    if ($ValidDirs -notcontains $ScriptDir) {
-        throw "Start-Scripts: '$ScriptDir' is not a valid script directory!"
-    }
-
-    #Use script directory as a reference position
-    Push-Location $PSScriptRoot
-
-    #Import all utils
-    Get-ChildItem -Path .\utils\*.ps1 | ForEach-Object { . $($_.FullName) }
-
-    # Get and run global scripts
-    Get-ChildItem ".\$ScriptDir\*.ps1" | ForEach-Object { & $($_.FullName) }
-
-    # Get and run game-specific scripts
-    Start-GameAction $Game.Name $ScriptDir
-
-    # Return to original directory
-    Pop-Location
-
-    # Wait for all jobs to complete before closing session
-    Get-Job | Wait-Job
+if ([string]::IsNullOrWhiteSpace($Directory)) {
+    throw "Start-Scripts: Please specify the value of '`$ScriptDir'!"
 }
+
+$ScriptDir = $Directory.toLower()
+if ($ValidDirs -notcontains $ScriptDir) {
+    throw "Start-Scripts: '$ScriptDir' is not a valid script directory!"
+}
+
+#Use script directory as a reference position
+Push-Location $PSScriptRoot
+
+#Import all utils
+Get-ChildItem -Path .\utils\*.ps1 | ForEach-Object { . $($_.FullName) }
+
+# Get and run global scripts
+Get-ChildItem ".\$ScriptDir\*.ps1" | ForEach-Object { & $($_.FullName) }
+
+# Get and run game-specific scripts
+Start-GameAction $Game.Name $ScriptDir
+
+# Return to original directory
+Pop-Location
+
+# Wait for all jobs to complete before closing session
+Get-Job | Wait-Job
